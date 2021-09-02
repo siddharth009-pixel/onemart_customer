5c16cdffb4eb82ac7840e01722173da2b4113556
Siddharth.Patel <opensiddhu993@gmail.com>
2021-09-02 03:18:03 -0700

    update urlconfig for api server link

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,6 @@
-export const api='http://localhost:2000/api'
+// export const api='http://localhost:5001/api'
+export const api='https://onemartbackend2.herokuapp.com/api'
+
 export const generatePublicUrl=(filename)=>{
     // return `http://localhost:2000/public/${filename}`
     return `${filename}`
