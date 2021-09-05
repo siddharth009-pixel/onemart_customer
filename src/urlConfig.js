905bbaff4f92be1db3b26ac550311b3062ca811b
Siddharth.Patel <opensiddhu993@gmail.com>
2021-09-05 11:30:45 -0700

    modified urlconfig

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,6 +1,6 @@
 // export const api='http://localhost:5001/api'
-export const api='https://fresh-liger-40.loca.lt/api'
-// export const api=process.env.API
+// export const api='https://great-falcon-86.loca.lt/api'
+export const api=process.env.API
 
 export const generatePublicUrl=(filename)=>{
     // return `http://localhost:2000/public/${filename}`
