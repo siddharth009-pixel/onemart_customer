2b3ff550d8b275ea9fdd59b44fd7e0e55250283e
Siddharth.Patel <opensiddhu993@gmail.com>
2021-09-05 10:44:36 -0700

    just modified api string

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 // export const api='http://localhost:5001/api'
-export const api='https://fresh-liger-40.loca.lt'
+export const api='https://fresh-liger-40.loca.lt/api'
 // export const api=process.env.API
 
 export const generatePublicUrl=(filename)=>{
