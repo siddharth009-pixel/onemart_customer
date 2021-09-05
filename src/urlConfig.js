be89f4e5723bf197dad8de1652fc351e06e83659
Siddharth.Patel <opensiddhu993@gmail.com>
2021-09-05 09:26:43 -0700

    update api with local serverlink

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 // export const api='http://localhost:5001/api'
-export const api='https://onemartbackend2.herokuapp.com/api'
+export const api='https://pink-grasshopper-84.loca.lt/api'
 
 export const generatePublicUrl=(filename)=>{
     // return `http://localhost:2000/public/${filename}`
