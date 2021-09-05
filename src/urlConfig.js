f88ee8928fd9a17cb90bf31a8481979951bbed89
Siddharth.Patel <opensiddhu993@gmail.com>
2021-09-05 10:21:14 -0700

    set api as a environment variable API

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,6 @@
 // export const api='http://localhost:5001/api'
-export const api='https://tasty-seahorse-21.loca.lt/api'
+// export const api='https://tasty-seahorse-21.loca.lt/api'
+export const api=process.env.API
 
 export const generatePublicUrl=(filename)=>{
     // return `http://localhost:2000/public/${filename}`
