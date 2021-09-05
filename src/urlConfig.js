3aecc0423c4bca1ed3a146c2339df3df5ed6f51f
Siddharth.Patel <opensiddhu993@gmail.com>
2021-09-05 10:00:01 -0700

    set api with another link

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 // export const api='http://localhost:5001/api'
-export const api='https://pink-grasshopper-84.loca.lt/api'
+export const api='https://tasty-seahorse-21.loca.lt/api'
 
 export const generatePublicUrl=(filename)=>{
     // return `http://localhost:2000/public/${filename}`
